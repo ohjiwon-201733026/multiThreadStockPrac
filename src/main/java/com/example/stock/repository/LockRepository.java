e690eb3bc782aba4c95c75c70142e555508e29af
ojiwon <ojw970725@gmail.com>
2024-12-19 04:11:21 +0900

    feature : Named lock 사용해 동시성 이슈 해결

---
+++ src/main/java/com/example/stock/repository/LockRepository.java
@@ -0,0 +1,15 @@
+package com.example.stock.repository;
+
+
+import com.example.stock.domain.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface LockRepository extends JpaRepository<Stock, Long> {    // 실무에서는 다른 DataSource 사용하기
+
+    @Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
+    void getLock(String key);
+
+    @Query(value = "select release_lock(:key)", nativeQuery = true)
+    void releaseLock(String key);
+}
---
+++ src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,57 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NamedLockStockFacadeTest {
+
+    @Autowired
+    private NamedLockStockFacade namedLockStockFacade;
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void setUp() {
+        stockRepository.save(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    namedLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();     // 다른 스레드에서 수행중인 작업 완료될 때까지 대기
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+    }
+
+}
