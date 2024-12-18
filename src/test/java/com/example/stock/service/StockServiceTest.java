46a9e9f69a32448517ae73a0626665e8933fa700
ojiwon <ojw970725@gmail.com>
2024-12-19 02:34:15 +0900

    feat : 재고 감소 로직

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -31,7 +31,6 @@ class StockServiceTest {
     @Test
     public void 재고감소() {
         stockService.decrease(1L, 1L);
-
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
